add49999ccd509c23e9ac87316a27b5fdf0d879c
JocelynSanchez11 <jocesanguev24@gmail.com>
2020-04-21 23:17:46 -0500

    Se subio proyecto libreria completo funcionando

---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+//Puerto
+process.env.POST = process.env.POST || 3000;
+
+//ENTORNO
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//firma secreta de jwt
+process.env.FIRMA = process.env.FIRMA || 'firma-bm24k';
+
+//CONECION A BASE DE DATOS
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') { //condicion para conectar con el cluster y si no se conecta localmente
+    urlDB = 'mongodb+srv://admin:admin@cluster0-fb2ad.mongodb.net/biblioteca?retryWrites=true&w=majority';
+} else {
+    urlDB = 'mongodb://localhost:27017/biblioteca';
+
+
+}
+
+process.env.URLDB = urlDB;
